1984db4c382b4b3546382f5f3ea1a222bf52c024
Saqib Ayoub <saqibtelixy@gmail.com>
2024-07-22 12:28:15 +0530

    made calculator

--- prac.cpp
+++ prac.cpp
@@ -11,11 +11,41 @@ double divi(int x, int y)
     {
         return x / y;
     }
+    return -1;
 }
 int mul(int x, int y)
 {
     return x * y;
 }
+int sub(int x, int y)
+{
+    return x - y;
+}
 int main()
 {
+
+    cout << "1.Add \n2.Sub \n3.Div \n4.Mul";
+    int n;
+    cin >> n;
+    int x, y;
+    cout << "Enter x and y";
+    cin >> x >> y;
+    switch (n)
+    {
+    case 1:
+        cout << add(x, y);
+        break;
+    case 2:
+        cout << sub(x, y);
+        break;
+    case 3:
+        cout << divi(x, y);
+        break;
+    case 4:
+        cout << mul(x, y);
+        break;
+
+    default:
+        break;
+    }
 }
